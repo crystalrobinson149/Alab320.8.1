4c31c4101d732347cd9447588544c318c2a03454
crystalrobinson149 <crystalrobinson149@gmail.com>
2025-03-14 12:33:41 -0400

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,39 +1,12 @@
-import {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom';
+
 
 function Main() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = await res.json();
-        console.log(data);
-        setUsers(data);
-      } catch (e) {
-        console.error(e)
-      }
-    }
-
-    fetchData();
-  }, [])
-
-  return (
-    <main>
-      <h1>Main Page</h1>
-
-      {users.map(user => {
-        return (
-          <div key={user.id}>
-            <Link to={`/profile/${user.id}`}>
-              <h2>{user.name}</h2>
-            </Link>
-          </div>
-        )
-      })}
-    </main>
-  );
+    
+    return(
+        <div>
+            <h1>This is the Main component</h1>
+        </div>
+    )
 }
 
 export default Main;
