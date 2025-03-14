8f9b6307ec868ef76bae477a6e37fc61aead5534
crystalrobinson149 <crystalrobinson149@gmail.com>
2025-03-14 12:37:54 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
+import './index.css' // "./style.css"
+import App from './App.jsx'
+// import BrowserRouter and rename it to Router
+import { BrowserRouter as Router } from "react-router-dom"
 
-createRoot(document.getElementById("root")).render(
+
+createRoot(document.getElementById('root')).render(
+
+// Wrap the App component with the Router component to enable the router features.
+// ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Router>
+    <App />
+    </Router>
   </StrictMode>,
-);
+  // document.getElementById("root")
+)
